90c95da8654391be24fc2a7a6a7c7c8fc89a2c2f
Ramzan Chasygov <theliketurbo@gmail.com>
2018-04-15 01:26:14 +0300

    first demo

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import './fonts/fonts.css';
-import 'b:Page';
-import App from 'b:App';
-
-ReactDOM.render(<App />, document.getElementById('root'));
